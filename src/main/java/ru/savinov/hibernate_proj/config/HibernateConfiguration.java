5a45ef5a90435dcc0bbacdd19430bae529ccbd29
savinov <savinov_vu@mail.ru>
2021-04-03 15:45:32 +0800

    Hibernate Configuration для настройки без spring boot

--- src/main/java/ru/savinov/hibernate_proj/config/HibernateConfiguration.java
+++ src/main/java/ru/savinov/hibernate_proj/config/HibernateConfiguration.java
@@ -1,13 +1,21 @@
 package ru.savinov.hibernate_proj.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Properties;
 
+/**
+ * Конфигурация этих бинов реализована в Spring boot по умолчанию
+ * */
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:db.properties")
@@ -16,6 +24,42 @@ public class HibernateConfiguration {
     @Autowired
     private Environment environment;
 
+    /**
+     * SessionFactory для подключения к базе данных
+     */
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(basicDataSource());
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+        localSessionFactoryBean.setPackagesToScan("ru.savinov.hibernate_proj.entity");
+        return localSessionFactoryBean;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory);
+        return hibernateTransactionManager;
+    }
+
+    /**
+     * настройки СУБД подключенной к проекту
+     */
+    @Bean
+    public BasicDataSource basicDataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        basicDataSource.setUrl(environment.getProperty("jdbc.url"));
+        basicDataSource.setUsername(environment.getProperty("jdbc.user"));
+        basicDataSource.setPassword(environment.getProperty("jdbc.pass"));
+        return basicDataSource;
+    }
+
+    /**
+     * настройки библиотеки (в данном проекте Hibernate) для реализации методов из спецификции JPF
+     */
     private Properties hibernateProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
