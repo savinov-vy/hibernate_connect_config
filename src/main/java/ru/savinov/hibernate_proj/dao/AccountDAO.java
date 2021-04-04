755c1b5babf8f794bbf84d5e31cb08984bd11d91
savinov <savinov_vu@mail.ru>
2021-04-04 09:47:54 +0800

    Add: Account (Entity, DAO)

---
+++ src/main/java/ru/savinov/hibernate_proj/dao/AccountDAO.java
@@ -0,0 +1,35 @@
+package ru.savinov.hibernate_proj.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.savinov.hibernate_proj.entity.Account;
+
+@Component
+public class AccountDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public AccountDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Account save(Account account) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(account);
+        session.flush();
+        session.getTransaction().commit();
+        session.close();
+        return account;
+    }
+
+    public Account findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Account account = session.find(Account.class, id);
+        session.close();
+        return account;
+    }
+}
---
+++ src/main/java/ru/savinov/hibernate_proj/entity/Account.java
@@ -0,0 +1,51 @@
+package ru.savinov.hibernate_proj.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long accountId;
+
+    private String name;
+
+    private Integer age;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
